4efae325ab7cd82580ea74c37da80c650f4942ac
Denys Fostytskyi <denys.fostytskyi@outlook.com>
2024-08-19 12:09:22 +0300

    Entities updated. Added seeding functionality for database

---
+++ Matchfinder/Entities/Photo.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Matchfinder.Entities
+{
+    [Table("Photos")]
+    public class Photo
+    {
+        public int Id { get; set; }
+        public required string Url { get; set; }
+        public bool IsMain { get; set; }
+        public string? PublicId { get; set; }
+
+        //Navigation Properties
+
+        // Required foreign key property
+        public int AppUserId { get; set; } 
+
+        // Required reference navigation for one to many relashions
+        public AppUser AppUser { get; set; } = null!; 
+    }
+}
--- Matchfinder/Program.cs
+++ Matchfinder/Program.cs
@@ -1,5 +1,7 @@
+using Matchfinder.Data;
 using Matchfinder.Extensions;
 using Matchfinder.Middleware;
+using Microsoft.EntityFrameworkCore;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -29,5 +31,21 @@ app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
+
+//Apply migrations at runtime
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+
+try
+{
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedUsers(context);
+}
+catch (Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occured during migration");
+}
 
 app.Run();
