1278d987fb6890c04a5f0709c4cdf6984b0a519e
Denys Fostytskyi <denys.fostytskyi@outlook.com>
2024-08-20 15:03:32 +0300

    Added Members and UI for Member Cards.

---
+++ client/src/app/_services/members.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { Member } from '../_models/member';
+import { AccountService } from './account.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MembersService {
+  private http = inject(HttpClient);
+  baseUrl = environment.apiUrl;
+  private accountService = inject(AccountService);
+
+  getMembers() {
+    return this.http.get<Member[]>(this.baseUrl + 'users', this.getHttpOptions());
+  }
+
+  getMember(username: string) {
+    return this.http.get<Member>(this.baseUrl + 'users/' + username, this.getHttpOptions());
+  }
+
+  getHttpOptions() {
+    return {
+      headers: new HttpHeaders({
+        Authorization: `Bearer ${this.accountService.currentUser()?.token}`
+      })
+    }
+  }
+}
