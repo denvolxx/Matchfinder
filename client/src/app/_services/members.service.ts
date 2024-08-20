ad1c2698a8c7d07eb53b77d295b99954126409cd
Denys Fostytskyi <denys.fostytskyi@outlook.com>
2024-08-20 16:01:41 +0300

    Added JWT interceptor for authorization

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -1,8 +1,7 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { Member } from '../_models/member';
-import { AccountService } from './account.service';
 
 @Injectable({
   providedIn: 'root'
@@ -10,21 +9,13 @@ import { AccountService } from './account.service';
 export class MembersService {
   private http = inject(HttpClient);
   baseUrl = environment.apiUrl;
-  private accountService = inject(AccountService);
 
   getMembers() {
-    return this.http.get<Member[]>(this.baseUrl + 'users', this.getHttpOptions());
+    return this.http.get<Member[]>(this.baseUrl + 'users');
   }
 
   getMember(username: string) {
-    return this.http.get<Member>(this.baseUrl + 'users/' + username, this.getHttpOptions());
+    return this.http.get<Member>(this.baseUrl + 'users/' + username);
   }
 
-  getHttpOptions() {
-    return {
-      headers: new HttpHeaders({
-        Authorization: `Bearer ${this.accountService.currentUser()?.token}`
-      })
-    }
-  }
 }
