d8a939f354fc5cdb160f05c5d64235a3a8059b4a
Denys Fostytskyi <denys.fostytskyi@outlook.com>
2024-08-22 14:44:11 +0300

    Feat: Photo upload API added

--- Matchfinder/Controllers/UsersController.cs
+++ Matchfinder/Controllers/UsersController.cs
@@ -1,14 +1,15 @@
 ﻿using AutoMapper;
 using Matchfinder.DTO;
+using Matchfinder.Entities;
+using Matchfinder.Extensions;
 using Matchfinder.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using System.Security.Claims;
 
 namespace Matchfinder.Controllers
 {
     [Authorize]
-    public class UsersController(IUserRepository userRepository, IMapper mapper) : BaseApiController
+    public class UsersController(IUserRepository userRepository, IMapper mapper, IPhotoService photoService) : BaseApiController
     {
         [HttpGet("")]
         public async Task<ActionResult<IEnumerable<MemberDTO>>> GetUsersAsync()
@@ -33,20 +34,51 @@ namespace Matchfinder.Controllers
         [HttpPut]
         public async Task<ActionResult> UpdateUser(MemberUpdateDTO member)
         {
-            var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-            if (username == null)
-                return BadRequest("No username found in token");
-
-            var user = await userRepository.GetUserByNameAsync(username);
+            var user = await userRepository.GetUserByNameAsync(User.GetUsername());
             if (user == null)
                 return BadRequest("Could not find user");
 
             mapper.Map(member, user);
 
             if (await userRepository.SaveAllAsync())
+            {
                 return NoContent();
+            }
+            else
+            {
+                return BadRequest("User was NOT updated");
+            }
+        }
 
-            return BadRequest("User was NOT updated");
+        [HttpPost("add-photo")]
+        public async Task<ActionResult<PhotoDTO>> AddPhoto(IFormFile file)
+        {
+            var user = await userRepository.GetUserByNameAsync(User.GetUsername());
+            if (user == null)
+                return BadRequest("Could not find user");
+
+            var result = await photoService.AddPhotoAsync(file);
+            if (result.Error != null)
+                return BadRequest(result.Error.Message);
+
+            var photo = new Photo
+            {
+                Url = result.SecureUrl.AbsoluteUri,
+                PublicId = result.PublicId,
+            };
+
+            user.Photos.Add(photo);
+
+            if (await userRepository.SaveAllAsync())
+            {
+                return CreatedAtAction(nameof(GetUser), new { username = user.UserName }, mapper.Map<PhotoDTO>(photo));
+            }
+            else
+            {
+                return BadRequest("Problem adding photo");
+            }
+
+
         }
     }
 }
---
+++ Matchfinder/Extensions/ClaimsPrincipleExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.Claims;
+
+namespace Matchfinder.Extensions
+{
+    public static class ClaimsPrincipleExtensions
+    {
+        public static string GetUsername(this ClaimsPrincipal user)
+        {
+            string? username = user.FindFirstValue(ClaimTypes.NameIdentifier);
+            if (username == null)
+            {
+                throw new Exception("Cannot get username from token");
+            }
+            return username;
+        }
+    }
+}
