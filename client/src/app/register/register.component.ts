27dd36a48074e2ab92bc8c0891afdcc19ae35972
Denys Fostytskyi <denys.fostytskyi@outlook.com>
2024-08-27 11:09:52 +0300

    Feat: Add registration UI using reactive forms

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,30 +1,64 @@
 import { Component, inject, input, output } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, ValidatorFn, Validators } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
 import { ToastrService } from 'ngx-toastr';
 import { AppComponent } from '../app.component';
+import { JsonPipe } from '@angular/common';
+import { TextInputComponent } from "../_forms/text-input/text-input.component";
+import { DatePickerComponent } from '../_forms/date-picker/date-picker.component';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [FormsModule],
+  imports: [ReactiveFormsModule, JsonPipe, TextInputComponent, DatePickerComponent],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
   private accountService = inject(AccountService);
   private toastr = inject(ToastrService);
+  private fb = inject(FormBuilder);
   cancelRegister = output<boolean>();
-  model: any = {}
+  model: any = {};
+  registerForm: FormGroup = new FormGroup({});
+  maxDate = new Date();
+
+  ngOnInit(): void {
+    this.intitializeForm();
+    this.maxDate.setFullYear(this.maxDate.getFullYear() - 18);
+  }
+
+  intitializeForm() {
+    this.registerForm = this.fb.group({
+      username: ['', Validators.required],
+      gender: ['male'],
+      knownAs: ['', Validators.required],
+      dateOfBirth: ['', Validators.required],
+      city: ['', Validators.required],
+      country: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(4)]],
+      confirmPassword: ['', [Validators.required, this.matchValues('password')]],
+    });
+    this.registerForm.controls['password'].valueChanges.subscribe({
+      next: () => this.registerForm.controls['confirmPassword'].updateValueAndValidity()
+    })
+  }
+
+  matchValues(matchTo: string): ValidatorFn {
+    return (control: AbstractControl) => {
+      return control.value === control.parent?.get(matchTo)?.value ? null : { isMatching: true }
+    }
+  }
 
   register() {
-    this.accountService.register(this.model).subscribe({
-      next: response => {
-        this.toastr.success(`User ${response.username} registered`)
-        this.cancel();
-      },
-      error: e => this.toastr.error(e.error)
-    })
+    console.log(this.registerForm.value)
+    // this.accountService.register(this.model).subscribe({
+    //   next: response => {
+    //     this.toastr.success(`User ${response.username} registered`)
+    //     this.cancel();
+    //   },
+    //   error: e => this.toastr.error(e.error)
+    // })
   }
 
   cancel() {
