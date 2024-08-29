602ae77363e96fd073eb9c0a6ee386f01b8113b0
Denys Fostytskyi <denys.fostytskyi@outlook.com>
2024-08-29 11:27:18 +0300

    Feat: Added pagination component. Implemented pagination for members on UI.

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -1,9 +1,10 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { inject, Injectable, signal } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { Member } from '../_models/member';
 import { of, tap } from 'rxjs';
 import { Photo } from '../_models/photo';
+import { PaginatedResult } from '../_models/paginations';
 
 @Injectable({
   providedIn: 'root'
@@ -11,56 +12,69 @@ import { Photo } from '../_models/photo';
 export class MembersService {
   private http = inject(HttpClient);
   baseUrl = environment.apiUrl;
-  members = signal<Member[]>([]);
+  //members = signal<Member[]>([]);
+  paginatedResult = signal<PaginatedResult<Member[]> | null>(null);
 
   //Get members and save them as a signal
-  getMembers() {
-    return this.http.get<Member[]>(this.baseUrl + 'users').subscribe({
-      next: members => this.members.set(members)
+  getMembers(pageNumber?: number, pageSize?: number) {
+    let params = new HttpParams();
+
+    if (pageNumber && pageSize) {
+      params = params.append('pageNumber', pageNumber);
+      params = params.append('pageSize', pageSize)
+    }
+
+    return this.http.get<Member[]>(this.baseUrl + 'users', { observe: 'response', params }).subscribe({
+      next: response => {
+        this.paginatedResult.set({
+          items: response.body as Member[],
+          pagination: JSON.parse(response.headers.get('Pagination')!)
+        })
+      }
     });
   }
 
   //Get member and save it as Observable
   getMember(username: string) {
-    const member = this.members().find(x => x.userName === username);
-    if (member !== undefined) return of(member);
+    // const member = this.members().find(x => x.userName === username);
+    // if (member !== undefined) return of(member);
 
     return this.http.get<Member>(this.baseUrl + 'users/' + username);
   }
 
   updateMember(member: Member) {
     return this.http.put(this.baseUrl + 'users', member).pipe(
-      tap(() => this.members.update(
-        members => members.map(m => m.userName === member.userName ? member : m)
-      ))
+      // tap(() => this.members.update(
+      //   members => members.map(m => m.userName === member.userName ? member : m)
+      // ))
     );
   }
 
   setMainPhoto(photo: Photo) {
     return this.http.put(this.baseUrl + 'users/set-main-photo/' + photo.id, {}).pipe(
-      tap(() => {
-        this.members.update(members => members.map(m => {
-          //Updates member in members signal with the new main photo
-          if (m.photos.includes(photo)) {
-            m.photoUrl = photo.url;
-          }
-          return m;
-        }))
-      })
+      // tap(() => {
+      //   this.members.update(members => members.map(m => {
+      //     //Updates member in members signal with the new main photo
+      //     if (m.photos.includes(photo)) {
+      //       m.photoUrl = photo.url;
+      //     }
+      //     return m;
+      //   }))
+      // })
     )
   }
 
   deletePhoto(photo: Photo) {
     return this.http.delete(this.baseUrl + 'users/delete-photo/' + photo.id).pipe(
-      tap(() => {
-        this.members.update(members => members.map(m => {
-          //Updates members signal, excludes members that are not deleted
-          if (m.photos.includes(photo)) {
-            m.photos = m.photos.filter(x => x.id !== photo.id)
-          }
-          return m
-        }))
-      })
+      // tap(() => {
+      //   this.members.update(members => members.map(m => {
+      //     //Updates members signal, excludes members that are not deleted
+      //     if (m.photos.includes(photo)) {
+      //       m.photos = m.photos.filter(x => x.id !== photo.id)
+      //     }
+      //     return m
+      //   }))
+      // })
     )
   }
 }
